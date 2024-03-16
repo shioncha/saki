afcad9c0b26be76bbec28f8acaaa38520bbbc52c
shioncha <57836246+shioncha@users.noreply.github.com>
2024-03-16 14:07:13 +0900

    Fix: Comments API

--- src/app/api/blog/[slug]/comments/route.ts
+++ src/app/api/blog/[slug]/comments/route.ts
@@ -3,16 +3,20 @@ import { Hono } from "hono";
 
 export const runtime = "edge";
 
-type Bindings = {
-  DB: D1Database
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB: D1Database;
+    }
+  }
 }
 
-const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
+const app = new Hono().basePath('/api');
 
 app.get('/blog/:slug/comments', async (c) => {
   const { slug } = c.req.param();
-  const { results } = await c.env.DB.prepare(
-    'SELECT yourName, yourEmail, comment, createdDate, createdTime FROM comments WHERE postId = ?'
+  const { results } = await process.env.DB.prepare(
+    'SELECT yourName, yourEmail, comment, createdDate, createdTime FROM blogComments WHERE postId = ?'
   )
     .bind(slug)
     .all();
@@ -23,8 +27,8 @@ app.get('/blog/:slug/comments', async (c) => {
 app.post('/blog/:slug/comments', async (c) => {
   const { slug } = c.req.param();
   const { yourName, yourEmail, comment } = await c.req.json();
-  const { success } = await c.env.DB.prepare(
-    'INSERT INTO comments (postId, yourName, yourEmail, comment, createdDate, createdTime) VALUES (?, ?, ?, ?, ?, ?)'
+  const { success } = await process.env.DB.prepare(
+    'INSERT INTO blogComments (postId, yourName, yourEmail, comment, createdDate, createdTime) VALUES (?, ?, ?, ?, ?, ?)'
   )
     .bind(slug, yourName, yourEmail, comment, new Date().toISOString().split('T')[0], new Date().toISOString().split('T')[1].split('.')[0])
     .run();
@@ -32,4 +36,5 @@ app.post('/blog/:slug/comments', async (c) => {
   return c.json({ id: success });
 })
 
-export default app;
+export const GET = app.fetch;
+export const POST = app.fetch;
