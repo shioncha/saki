c5befca976eefbddad002ffc71ad31251bd21a28
shioncha <57836246+shioncha@users.noreply.github.com>
2024-04-02 01:32:48 +0900

    Revert "Revert "Fix: Comments component""
    This reverts commit 0ec2722fe7366f7acd0c8fc033e2d250c764cae9.

--- src/components/elements/comments/index.jsx
+++ src/components/elements/comments/index.jsx
@@ -27,7 +27,9 @@ export function Comments({ postId }) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(formData),
-    });
+        });
+
+        setFormData({ yourName: '', yourEmail: '', comment: '' });
     }
 
     useEffect(() => {
