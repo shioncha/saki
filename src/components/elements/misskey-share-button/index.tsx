e830a526b88cd2b1c0b13389f41c983da3cda6dc
shioncha <57836246+shioncha@users.noreply.github.com>
2024-10-05 21:23:10 +0900

    Update: misskey-share-button component - リンクではなくボタン要素を使用するように - その他、軽微な修正

--- src/components/elements/misskey-share-button/index.tsx
+++ src/components/elements/misskey-share-button/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { SiMisskey } from "react-icons/si";
 import styles from "./MisskeyShare.module.css";
 
@@ -8,18 +7,14 @@ interface Props {
 }
 
 export function MisskeyShare({ url, title }: Props): JSX.Element | null {
-    if (!url || !title) {
-        return null;
-    }
+    if (!url || !title) return null;
 
     const encodedUrl = encodeURIComponent(url);
-    const link = `https://misskey-hub.net/share/?text=[${title}](${encodedUrl})&visibility=public&localOnly=0&manualInstance=https:%2F%2Fmisskey.backspace.fm`;
+    const link = `https://misskey-hub.net/share/?text=[${title}](${encodedUrl})&visibility=public&localOnly=0`;
 
     return (
-        <Link href={link} target="_blank" rel="noopener noreferrer">
-            <div className={styles.icon}>
-                <SiMisskey color='white' size={24}/>
-            </div>
-        </Link>
+        <button className={styles.icon} onClick={() => window.open(link, '_blank')}>
+            <SiMisskey color='white' size={24}/>
+        </button>
     );
 }
