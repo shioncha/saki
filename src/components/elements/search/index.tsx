d83e63ed2d281f3ddf957e68c0cc0ea94e5cb6a8
shioncha <57836246+shioncha@users.noreply.github.com>
2024-10-21 00:40:45 +0900

    Fix: fix and update article search function
    - 引数にsearchParams(を追加するとなんか直った)
    - 一部をSSGに変更
    - コンポーネントの分割

---
+++ src/components/elements/search/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { CiSearch } from 'react-icons/ci';
+import { DateFormatter } from '@/components/elements/dateFormatter';
+import styles from './SearchFunc.module.css';
+
+interface Article {
+  id: string;
+  title: string;
+  publishedAt: string;
+}
+
+export default function SearchFunc() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  async function handler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const q = (event.target as HTMLFormElement).q.value;
+    const params = new URLSearchParams(searchParams);
+    if (!q) {
+      setArticles([]);
+      replace('/search');
+      return;
+    }
+    params.set('q', q);
+    replace(`/search?${params.toString()}`);
+  }
+
+  useEffect(() => {
+    const search = async () => {
+      setLoading(true);
+      const q = searchParams.get('q');
+      const res = await fetch(`/api/blog/search?q=${q}`);
+      const json = await res.json();
+      setArticles(json);
+      setLoading(false);
+    }
+    if (searchParams.get('q')) search();
+  }, [searchParams]);
+
+  return (
+    <>
+      <form onSubmit={handler} className={styles.searchBox}>
+        <input type="text" name="q" placeholder='Search' defaultValue={searchParams.get('q')?.toString()} className={styles.input} />
+        <button type="submit" className={styles.button}><CiSearch size={24} color='white'/></button>
+      </form>
+      {loading ? <p>Loading...</p> : (
+        <div className={styles.articles}>
+          {articles.map((article) => {
+            const url = `/blog/${article.id}`;
+            return (
+              <div key={article.id} className={styles.article}>
+                <p><Link href={url} target="_blank">{article.title}</Link></p>
+                <DateFormatter date={article.publishedAt} />
+              </div>
+            );
+          }, [])}
+        </div>
+      )}
+    </>
+  );
+}
