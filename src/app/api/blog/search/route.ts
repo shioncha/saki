b04c8aadf1f09b072c0273bde7e1a0c67c0a23ed
shioncha <57836246+shioncha@users.noreply.github.com>
2024-10-21 01:50:35 +0900

    test

--- src/app/api/blog/search/route.ts
+++ src/app/api/blog/search/route.ts
@@ -7,7 +7,14 @@ export const runtime = "edge";
 
 const app = new Hono().basePath('/api');
 
-app.get('/blog/search', csrf(), async (c) => {
+app.use(csrf({
+  origin: [
+    'https://dev.saki.pages.dev',
+    'https://saki.pages.dev',
+  ]
+}));
+
+app.get('/blog/search', async (c) => {
   const query = c.req.query('q');
   const queries: MicroCMSQueries = {
     fields: 'id,title,publishedAt',
