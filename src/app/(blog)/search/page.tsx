89a8124a9bdf488b5625f5cf3d5fd9d34439211f
shioncha <57836246+shioncha@users.noreply.github.com>
2024-11-04 21:35:31 +0900

    suspense

--- src/app/(blog)/search/page.tsx
+++ src/app/(blog)/search/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import styles from './page.module.css';
 import SearchFunc from '@/components/elements/search';
 
 export default function Page() {
-  useEffect(() => {
-  }, []);
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Search</h1>
       <p>記事の内容から全文検索ができます。検索結果はヒットした記事のうち、最新の12件まで表示されます。</p>
-      <SearchFunc />
+      <Suspense>
+        <SearchFunc />
+      </Suspense>
     </div>
   );
 }
