e367f4e9671f7b8f831b74e95b8a03358b1e3a9e
shioncha <57836246+shioncha@users.noreply.github.com>
2024-11-04 23:02:00 +0900

    FIx: search function
    - APIがCloudflare Workersで動くよう修正
    - Suspenseを利用するように修正

--- src/app/(blog)/search/page.tsx
+++ src/app/(blog)/search/page.tsx
@@ -1,5 +1,6 @@
 export const runtime = 'edge';
 
+import { Suspense } from 'react';
 import styles from './page.module.css';
 import SearchFunc from '@/components/elements/search';
 
@@ -8,7 +9,9 @@ export default function Page() {
     <div className={styles.container}>
       <h1 className={styles.title}>Search</h1>
       <p>記事の内容から全文検索ができます。検索結果はヒットした記事のうち、最新の12件まで表示されます。</p>
-      <SearchFunc />
+      <Suspense>
+        <SearchFunc />
+      </Suspense>
     </div>
   );
 }
